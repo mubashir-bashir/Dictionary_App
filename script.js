c13b1284ca3cfe600558ce8f511b7602dd60ba3f
Mubashir <mb.mubashirbashir@gmail.com>
2023-07-10 14:56:10 +0500

    initilize

---
+++ script.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('form');
+const resultDiv = document.querySelector('.result');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getWordInfo(form.elements[0].value);
+});
+
+const getWordInfo = async (word) => {
+  try {
+    resultDiv.innerHTML= "Fetching Data....";
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+    const data = await response.json();
+    let definitions = data[0].meanings[0].definitions[0];
+    resultDiv.innerHTML = `
+      <h2><strong>Word:</strong>${data[0].word}</h2>
+      <p class='partOfSpeech'>${data[0].meanings[0].partOfSpeech}</p>
+      <p><strong>Meaning:</strong>${definitions.definition === undefined ? "Not Found" : definitions.definition}</p>
+      <p><strong>Example:</strong>${definitions.example === undefined ? "Not Found" : definitions.example}</p>
+    `;
+
+    // Fetching Antonyms 
+    if (definitions.antonyms.length === 0) {
+      resultDiv.innerHTML += `<p><strong>Antonyms:</strong> Not Found</p>`;
+    } else {
+      resultDiv.innerHTML += `<p><strong>Antonyms:</strong></p>`;
+      for (let i = 0; i < definitions.antonyms.length; i++) {
+        resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`;
+      }
+    }
+
+    // Fetching Synonyms 
+    if (definitions.synonyms.length === 0) {
+      resultDiv.innerHTML += `<p><strong>Synonyms:</strong> Not Found</p>`;
+    } else {
+      resultDiv.innerHTML += `<p><strong>Synonyms:</strong></p>`;
+      for (let i = 0; i < definitions.synonyms.length; i++) {
+        resultDiv.innerHTML += `<li>${definitions.synonyms[i]}</li>`;
+      }
+    }
+
+    // Adding Read More Buttons 
+    resultDiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More</a></div>`;
+  } catch (error) {
+    resultDiv.innerHTML = `<p>Sorry, the word could not be found</p>`;
+  }
+};
+
